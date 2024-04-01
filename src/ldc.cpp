98b29b2d2fb47fdffdf43902b356284390cfbb5c
maknee <zhu109@illinois.edu>
2024-03-31 21:25:57 -0600

    logs

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -432,18 +432,18 @@ int main(int argc, char *argv[])
     }
     default_value = std::string(ops_config.VALUE_SIZE, 'A');
     auto value = default_value;
-    for (const auto &k : keys)
-    {
-      auto key_index = std::stoi(k);
-      if (key_index >= start_keys && key_index < end_keys)
-      {
-        block_cache->put(k, value);
-      }
-      else
-      {
-        block_cache->get_db()->put(k, value);
-      }
-    }
+    // for (const auto &k : keys)
+    // {
+    //   auto key_index = std::stoi(k);
+    //   if (key_index >= start_keys && key_index < end_keys)
+    //   {
+    //     block_cache->put(k, value);
+    //   }
+    //   else
+    //   {
+    //     block_cache->get_db()->put(k, value);
+    //   }
+    // }
 
     // Connect to one sided RDMA
     if (config.baseline.one_sided_rdma_enabled)
--- src/ldc.h
+++ src/ldc.h
@@ -369,7 +369,7 @@ struct CacheIndexes : public RDMAData
       auto size = rdma_kv_storage->get_allocated_cache_index_size();
       auto offset = key_index * sizeof(RDMACacheIndex);
       const auto& rdma_cache_index = rdma_cache_indexes[machine_index];
-      LOG_RDMA_DATA("[CacheIndexes] Writing remote {} key {} offset {}", i, key_index, rdma_cache_index[key_index].key_value_ptr_offset);
+      LOG_RDMA_DATA("[CacheIndexes] Writing remote {} key {} offset {} {}", i, key_index, (void*)&rdma_cache_index[key_index], rdma_cache_index[key_index].key_value_ptr_offset);
       auto request_token = RDMAData::write(i, rdma_cache_index, size, offset, offset, sizeof(RDMACacheIndex));      
       pending_write_queue.enqueue(request_token);
     }
