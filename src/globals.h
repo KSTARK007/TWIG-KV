7086fb9daa4281b568868ea88741f3fb30d224e9
KSTARK007 <kiranhombal98@gmail.com>
2024-04-09 02:38:31 -0600

    removed logs

--- src/globals.h
+++ src/globals.h
@@ -7,12 +7,12 @@
 #include <condition_variable>
 
 // Defines
-// #define LOG_STATE(...)
-#define LOG_STATE info
+#define LOG_STATE(...)
+// #define LOG_STATE info
 // #define LOG_STATE(...) debug(__VA_ARGS__)
 // #define LOG_STATE(...) info(__VA_ARGS__)
-// #define LOG_RDMA_DATA
-#define LOG_RDMA_DATA info
+#define LOG_RDMA_DATA
+// #define LOG_RDMA_DATA info
 
 #define BLKSZ 4096
 
