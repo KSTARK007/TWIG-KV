42fbeb29670e7c5024bb86f18903114d66bda8eb
KSTARK007 <kiranhombal98@gmail.com>
2024-04-09 12:48:27 -0600

    Refactor server_worker function in ldc.cpp to include additional parameters

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -374,7 +374,7 @@ void server_worker(
                 {
                   if (config.baseline.one_sided_rdma_enabled && config.baseline.use_cache_indexing)
                   {
-                    found_in_rdma = rdma_node.rdma_key_value_cache->read_callback(key_index, [&, remote_index, remote_port, expected_key=key_index](const RDMACacheIndexKeyValue& kv)
+                    found_in_rdma = rdma_node.rdma_key_value_cache->read_callback(key_index, [=, &server, &block_cache, &fetch_from_disk, base_index, remote_index, remote_port, expected_key=key_index](const RDMACacheIndexKeyValue& kv)
                     {
                       total_rdma_executed.fetch_add(1, std::memory_order::relaxed);
                       uint64_t key_index = kv.key_index;
