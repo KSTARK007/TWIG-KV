387a5799323476786171b3e3d3cafc296e861206
makneee <makneee@node14.large-c-2.rasl-pg0.utah.cloudlab.us>
2024-04-17 11:48:48 -0600

    Log

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -759,14 +759,17 @@ int main(int argc, char *argv[])
         cache_info.at("reads").get_to(current_cache_reads);
         cache_info.at("cache_hit").get_to(current_cache_hits);
         cache_info.at("cache_miss").get_to(current_cache_misses);
+        auto diff_cache_reads = current_cache_reads - last_cache_reads;
+        auto diff_cache_hits = current_cache_hits - last_cache_hits;
+        auto diff_cache_misses = current_cache_misses - last_cache_misses;
 
         info("Ops [{}] +[{}] | RDMA [{}] +[{}] | Disk [{}] +[{}] | C Read [{}] +[{}] | C Hit [{}] +[{}] | C Miss [{}] +[{}]", 
             current_rdma_executed, diff_rdma_executed,
             current_ops_executed, diff_ops_executed,
             current_disk_executed, diff_disk_executed,
-            current_cache_reads, last_cache_reads,
-            current_cache_hits, last_cache_hits,
-            current_cache_misses, last_cache_misses
+            current_cache_reads, diff_cache_reads,
+            current_cache_hits, diff_cache_hits,
+            current_cache_misses, diff_cache_misses
         );
 
         last_rdma_executed = current_rdma_executed;
