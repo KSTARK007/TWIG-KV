20e9b4fa1925b6fe30a87a0cf997392394e0a35b
KSTARK007 <kiranhombal98@gmail.com>
2024-05-01 14:19:24 -0600

    Refactor access_rate_calculations.cpp to set new optimized water marks and access rate, and update cache keys from past

--- src/access_rate_calculations.cpp
+++ src/access_rate_calculations.cpp
@@ -316,6 +316,7 @@ void get_best_access_rates(std::shared_ptr<BlockCache<std::string, std::string>>
     set_water_marks(cache, best_local, best_remote);
     uint64_t best_access_rate = (cdf[best_local].first) * 0.90;
     cache->get_cache()->set_access_rate(best_access_rate);
+    cache->get_cache()->set_keys_from_past(cdf);
     
     // // Get and set keys under L
     // std::vector<std::string> keys_under_l = get_keys_under_l(cdf, best_local);
