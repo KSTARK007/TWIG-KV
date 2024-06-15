406ae33aa046636a3b8a5767c66b375ac7c6c0a5
Kiran S Hombal <kiranhombal98@gmail.com>
2024-06-15 18:10:46 -0500

    Refactor ldc.cpp to use absolute file path for operations

--- setup/run_compile.sh
+++ setup/run_compile.sh
@@ -8,17 +8,18 @@ GIT_SSH_KEY_PATH="--git_ssh_key_path /users/Khombal2/.ssh/id_rsa"
 NUM_SERVERS="--num_servers 3"
 # POLICY_TYPES=("thread_safe_lru" "nchance" "access_rate")
 # CACHE_SIZE=(0.10 0.15 0.20 0.25 0.30 0.334 0.35)
-CACHE_SIZE=(0.10 0.15 0.20 0.25 0.30 0.334)
+# CACHE_SIZE=(0.10 0.15 0.20 0.25 0.30 0.334)
 # CACHE_SIZE=(0.334 0.35)
 # CACHE_SIZE=(0.10 0.15)
 # SYSTEM_NAMES=("A" "B")
 
 # CACHE_SIZE=(0.25 0.334)
-# CACHE_SIZE=(0.10)
+CACHE_SIZE=(0.10)
 # SYSTEM_NAMES=("A" "B" "C")
 SYSTEM_NAMES=("C")
 POLICY_TYPES=("access_rate")
-ACCESS_RATE=(100 200 250 400 500 750 1000 1500 2000 2500 3000 3500 4000)
+# ACCESS_RATE=(100 200 250 400 500 750 1000 1500 2000 2500 3000 3500 4000)
+ACCESS_RATE=(100)
 # A : Cache Unaware Routing + Admit + Disk
 # B : Cache Unaware Routing + no-Admit + Disk
 # C : Cache Unaware Routing + no-Admit + RDMA = old(LDC)
@@ -43,11 +44,12 @@ execute_cmd_with_timeout() {
     --num_threads $num_threads --policy $policy --system_type $system_name --distribution $distribution --num_clients $num_clients \
     --num_clients_per_thread $num_clients_per_thread --git_branch LDC_DYNAMIC --cache_size $cache_size_for_this_run --access_rate $access_rate --workload $WORKLOAD"
     echo "Executing: $CMD"
+    eval $CMD
 
-    timeout 15m bash -c "eval $CMD" || (
-        echo "Command timed out after 15 minutes. Restarting..."
-        execute_cmd_with_timeout $num_clients $num_threads $num_clients_per_thread $step $distribution $system_name $policy $cache_size_for_this_run $access_rate $WORKLOAD
-    )
+    # timeout 15m bash -c "eval $CMD" || (
+    #     echo "Command timed out after 15 minutes. Restarting..."
+    #     execute_cmd_with_timeout $num_clients $num_threads $num_clients_per_thread $step $distribution $system_name $policy $cache_size_for_this_run $access_rate $WORKLOAD
+    # )
 }
 
 execute_cmd() {
@@ -67,6 +69,35 @@ execute_cmd() {
 }
 
 # Function to iterate through ranges and execute commands
+iterate_and_execute_setup() {
+    local num_clients_start=$1
+    local num_clients_end=$2
+    local num_threads_start=$3
+    local num_threads_end=$4
+    local num_clients_per_thread_start=$5
+    local num_clients_per_thread_end=$6
+    local distribution=$7
+    local system_name=$8
+    local policy=$9
+    local access_rate=${10}
+    
+    for ((num_clients=num_clients_start; num_clients<=num_clients_end; num_clients++)); do
+        for ((num_threads=num_threads_start; num_threads<=num_threads_end; num_threads++)); do
+            for ((num_clients_per_thread=num_clients_per_thread_start; num_clients_per_thread<=num_clients_per_thread_end; num_clients_per_thread++)); do
+                # execute_cmd $num_clients $num_threads $num_clients_per_thread 0 $distribution $system_name $policy $access_rate
+                # execute_cmd $num_clients $num_threads $num_clients_per_thread 1 $distribution $system_name $policy $access_rate
+                # execute_cmd $num_clients $num_threads $num_clients_per_thread 2 $distribution $system_name $policy $access_rate
+
+                execute_cmd $num_clients $num_threads $num_clients_per_thread 3 $distribution $system_name $policy $access_rate
+                execute_cmd $num_clients $num_threads $num_clients_per_thread 4 $distribution $system_name $policy $access_rate
+                execute_cmd $num_clients $num_threads $num_clients_per_thread 6 $distribution $system_name $policy $access_rate
+                execute_cmd $num_clients $num_threads $num_clients_per_thread 7 $distribution $system_name $policy $access_rate
+                execute_cmd $num_clients $num_threads $num_clients_per_thread 11 $distribution $system_name $policy $access_rate
+            done
+        done
+    done
+}
+
 iterate_and_execute() {
     local num_clients_start=$1
     local num_clients_end=$2
@@ -123,38 +154,19 @@ iterate_and_execute() {
 # POLICY_TYPES=("thread_safe_lru")
 # ACCESS_RATE=(50)
 
-CACHE_SIZE=(0.334)
-SYSTEM_NAMES=("C")
-POLICY_TYPES=("access_rate_dynamic")
-ACCESS_RATE=(50)
-
 WORKLOAD="YCSB"
-# Execute with specified ranges and steps
-for system_name in "${SYSTEM_NAMES[@]}"; do
-    if [[ $system_name == "C" ]]; then
-        for policy in "${POLICY_TYPES[@]}"; do
-            if [[ $policy == "access_rate" ]]; then
-                for access_rate in "${ACCESS_RATE[@]}"; do
-                    iterate_and_execute 3 3 8 8 2 2 "hotspot" $system_name $policy $access_rate
-                done
-            else
-                access_rate=4000
-                iterate_and_execute 3 3 8 8 2 2 "hotspot" $system_name $policy $access_rate
-            fi
-        done
-    else
-        iterate_and_execute 3 3 8 8 2 2 "hotspot" $system_name "thread_safe_lru" 0
-    fi
-done
+iterate_and_execute_setup 3 3 8 8 2 2 "hotspot" "C" "access_rate_dynamic" 50
+# iterate_and_execute 3 3 8 8 2 2 "hotspot" $system_name $policy $access_rate
 
-# CACHE_SIZE=(0.15 0.20 0.25 0.30)
-# # SYSTEM_NAMES=("A" "B" "C")
-# SYSTEM_NAMES=("B" "C")
-# POLICY_TYPES=("thread_safe_lru" "nchance" "access_rate")
-# ACCESS_RATE=(50 5000)
+# CACHE_SIZE=(0.334)
+# SYSTEM_NAMES=("C")
+# POLICY_TYPES=("access_rate_dynamic")
+# ACCESS_RATE=(50)
 
 # WORKLOAD="YCSB"
 # # Execute with specified ranges and steps
+
+
 # for system_name in "${SYSTEM_NAMES[@]}"; do
 #     if [[ $system_name == "C" ]]; then
 #         for policy in "${POLICY_TYPES[@]}"; do
