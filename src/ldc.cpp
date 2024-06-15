653e0fb1ad5dd63c9ad1d0f4a9e0f89fa33ba421
KSTARK007 <kiranhombal98@gmail.com>
2024-06-15 16:41:12 -0600

    Refactor ldc.cpp to use absolute file path for operations

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -228,7 +228,7 @@ void client_worker(std::shared_ptr<Client> client_, BlockCacheConfig config, Con
   if (ops_config.DISTRIBUTION_TYPE != DistributionType::YCSB) {
     ops_chunk = get_chunk(ops, FLAGS_threads * FLAGS_clients_per_threads, client_index);
   } else {
-    std::string file_name = "client_" + std::to_string(machine_index) + "_thread_" + std::to_string(thread_index) + "_clientPerThread_" + std::to_string(client_index_per_thread) + ".txt";
+    std::string file_name = "/mydata/client_" + std::to_string(machine_index) + "_thread_" + std::to_string(thread_index) + "_clientPerThread_" + std::to_string(client_index_per_thread) + ".txt";
     info("Using {} file for operations on client {} thread {} clientPerThread {}", file_name, machine_index, thread_index, client_index_per_thread);
     ops_chunk = loadOperationSetFromFile(file_name);
   }
